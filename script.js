f418bcb806fd2bdcc166e432d10485fa4344873d
Nayeem Ahmed <134209701+NayeemAhmed23@users.noreply.github.com>
2023-11-20 18:55:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+// Create the student object with a 'name' property
+const student = {
+    name: 'John Doe',
+};
+
+// Add the getKeys() method to the Object prototype
+Object.prototype.getKeys = function () {
+    // Use Object.keys() to get an array of keys from the current object
+    return Object.keys(this);
+};
